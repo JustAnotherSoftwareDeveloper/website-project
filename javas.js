7d0d4439ec285a3974c7a13af3eeb3a297b934a9
Michael Sila <msila@arbfund.com>
2016-07-01 14:50:14 -0600

    adding existing stuff

---
+++ javas.js
@@ -0,0 +1,14 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav(
+        {
+            menuWidth: 500, // Default is 240
+            edge: 'left', // Choose the horizontal origin
+            closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        }
+    
+    );
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
