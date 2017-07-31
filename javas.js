97a1ff3f0b08ef302046e454104093bb12447ecf
Michael Sila <michael.sila.career@outlook.com>
2017-07-30 19:39:32 -0600

    cleanup

--- javas.js
+++ javas.js
@@ -1,14 +1,7 @@
-(function( $ ){
-	$(function(){
-
-		$('.button-collapse').sideNav();
-
-	}); // end of document ready
-})(jQuery); // end of jQuery name space
-
 $(document).ready(function () {
 	// Plugin initialization
 	$('.slider').slider();
+	$('.button-collapse').sideNav();
     
 
 });
